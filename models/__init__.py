b4fd65695e12fbb20240f9449d8adff6e3b21558
Mathias Villerabel <m.villerabel@gmail.com>
2025-09-28 16:51:47 +0200

    adding 3 models

--- models/__init__.py
+++ models/__init__.py
@@ -4,8 +4,14 @@ Models package for movie recommendation system.
 
 from .model import BaseModel
 from .collaborative.collaborative_filtering import CollaborativeFilteringModel
+from .content_based.content_based_model import ContentBasedModel
+from .hybrid.hybrid_model import HybridModel
+from .deep.deep_collaborative_filtering import DeepCollaborativeFiltering
 
 __all__ = [
-    "BaseModel",
-    "CollaborativeFilteringModel",
+    'BaseModel',
+    'CollaborativeFilteringModel',
+    'ContentBasedModel',
+    'HybridModel',
+    'DeepCollaborativeFiltering',
 ]
